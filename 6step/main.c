498517aad3165066a8c1ddf7aa060d6383b3e1b3
Elias SACI <esaci@e1r9p20.42.fr>
2020-09-18 00:34:48 +0200

    ok

--- 6step/main.c
+++ 6step/main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <mlx.h>
 #include <string.h>
+#include <math.h>
 
 /* OUBLIE PAS DE CHANGE LE MEMCPY PAR FTMEMCPY*/
+/* Utilise fabs = valeur absolu */
 
 # define X_EVENT_KEY_PRESS			2
 # define X_EVENT_KEY_release		3
@@ -20,6 +22,7 @@
 # define COLS						15
 # define WIDTH						COLS * 32
 # define HEIGHT						ROWS * 32
+# define TO_COORD(X, Y) ((int)floor(Y) * WIDTH + (int)floor(X))
 
 typedef struct s_img
 {
@@ -40,15 +43,15 @@ typedef struct	s_mlx
 
 typedef struct	s_game
 {
-	t_mlx	*mlx;
+	t_mlx	mlx;
 	t_img	img;
 	int		map[ROWS][COLS];
 }				t_game;
 
 void	window_init(t_game *game)
 {
-	game->mlx->mlxptr = mlx_init();
-	game->mlx->win = mlx_new_window(game->mlx->mlxptr, WIDTH, HEIGHT, "ptit tableau tah elias");
+	game->mlx.mlxptr = mlx_init();
+	game->mlx.win = mlx_new_window(game->mlx.mlxptr, WIDTH, HEIGHT, "ptit tableau tah elias");
 	return;
 }
 
@@ -57,16 +60,110 @@ void	map_init(t_game *game)
 	int	map[ROWS][COLS] = {
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1},
+	{1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1},
 	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	};
 	memcpy(game->map, map, sizeof(int) * ROWS * COLS);
+}
+
+void	img_init(t_game *game)
+{
+	game->img.imgptr = mlx_new_image(game->mlx.mlxptr, WIDTH, HEIGHT);
+	game->img.data = (int *)mlx_get_data_addr(game->img.imgptr, &game->img.bpp, &game->img.size_l, &game->img.endian);
+}
+
+void	ftrectangle(t_game *game, int x, int y)
+{
+	int i;
+	int j;
+
+	x *= 32;
+	y *= 32;
+	i = 0;
+	while (i < 32)
+	{
+		j = 0;
+		while (j < 32)
+		{
+			game->img.data[(y + i) * WIDTH + x + j] = 0xFF000;
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ftrectangleur(t_game *game)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < ROWS)
+	{
+		j = 0;
+		while(j < COLS)
+		{
+			if (game->map[i][j] == 0)
+				ftrectangle(game, j, i);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ftligne(t_game *game, double x1, double y1, double x2, double y2)
+{
+	double deltaX;
+	double deltaY;
+	double step;
+
+	deltaX = x2 - x1;
+	deltaY = y2 - y1;
+	step = (fabs(deltaX) > fabs(deltaY)) ? fabs(deltaX) : fabs(deltaY);
+	deltaX /= step;
+	deltaY /= step;
+	while (fabs(x2 - x1) > 0.01 || fabs(y2- y1) > 0.01)
+	{
+		game->img.data[TO_COORD(x1, y1)] = 0xb3b3b3;
+		x1 += deltaX;
+		y1 += deltaY;
+	}
+}
+
+void	ligneur(t_game *game)
+{
+	int		i;
+	int		j;
+
+	i = 0;
+	while (i < COLS)
+	{
+		ftligne(game, i * 32 - 1, 0, i * 32 - 1, HEIGHT);
+		i++;
+	}
+	ftligne(game, COLS * 32 - 1, 0, COLS * 32 - 1, HEIGHT);
+	j = 0;
+	while (j < ROWS)
+	{
+		ftligne(game, 0, j * 32, WIDTH, j * 32);
+		j++;
+	}
+	ftligne(game, 0, ROWS * 32 - 1, WIDTH, ROWS * 32 - 1);
+}
+
+int		mappeur(t_game *game)
+{
+	ftrectangleur(game);
+	ligneur(game);
+	mlx_put_image_to_window(game->mlx.mlxptr, game->mlx.win, game->img.imgptr, 0, 0);
+	return (0);
 }
 
 int	main(void)
@@ -75,5 +172,9 @@ int	main(void)
 
 	map_init(&game);
 	window_init(&game);
+	img_init(&game);
+
+	mlx_loop_hook(game.mlx.mlxptr, &mappeur, &game);
+	mlx_loop(game.mlx.mlxptr);
 	return (0);
 }
